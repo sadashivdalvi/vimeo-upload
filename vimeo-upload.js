27fa4a20e915b2683b4118de97f041e058575418
Sadashiv Dalvi <dalvisadashiv@gmail.com>
2025-02-12 18:19:15 +0530

    Fixing privacy setting

--- vimeo-upload.js
+++ vimeo-upload.js
@@ -162,7 +162,7 @@
         this.videoData = {
             name: (opts.name > '') ? opts.name : defaults.name,
             description: (opts.description > '') ? opts.description : defaults.description,
-            'privacy.view': opts.private ? 'nobody' : 'anybody'
+            'privacy': {'view': opts.private ? opts.private : 'anybody'}
         }
 
         if (!(this.url = opts.url)) {
